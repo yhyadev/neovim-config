81abff9e1b0b4a25f0f4b77fbcb70ec2a39ab417
Yhya <yhyamobile57@gmail.com>
2023-12-19 20:22:04 +0200

    add the mit license as a snippet

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -5,7 +5,8 @@ if not snip_status_ok then
 	return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/lua/configs/luasnip/snippets/" } })
 
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
